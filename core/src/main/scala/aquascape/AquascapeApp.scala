8e0fbcc1ef52a404e3dc1929ec63e20745d2c2ee
zainab-ali <zainab@kebab-ca.se>
2024-03-16 00:29:44 +0000

    Add app.

---
+++ core/src/main/scala/aquascape/AquascapeApp.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 Zainab Ali
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aquascape
+
+import cats.effect.*
+import aquascape.drawing.Config
+import doodle.java2d.*
+import doodle.syntax.all.*
+import doodle.core.format.*
+
+object AquascapeApp {
+  trait Core {
+    def config: Config = Config.default
+
+    def stream(using Stage[IO]): IO[Unit]
+  }
+
+  trait Simple extends IOApp.Simple with Core {
+    def run: IO[Unit] = Stage
+      .unchunked[IO]
+      .flatMap { case t @ given Stage[IO] =>
+        stream(using t).draw()
+      }
+      .flatMap(_.drawToIO())
+  }
+  object Simple {
+    abstract class File(name: String) extends IOApp.Simple with Core {
+      def run: IO[Unit] = Stage
+        .unchunked[IO]
+        .flatMap { case t @ given Stage[IO] =>
+          stream(using t).draw()
+        }
+        .flatMap(_.writeToIO[Png](s"$name.png"))
+    }
+  }
+
+  trait Chunked extends IOApp.Simple with Core {
+    def run: IO[Unit] = Stage
+      .unchunked[IO]
+      .flatMap { case t @ given Stage[IO] =>
+        stream(using t).draw()
+      }
+      .flatMap(_.drawToIO())
+  }
+
+  object Chunked {
+    abstract class File(name: String) extends IOApp.Simple with Core {
+      def run: IO[Unit] = Stage
+        .chunked[IO]
+        .flatMap { case t @ given Stage[IO] =>
+          stream(using t).draw()
+        }
+        .flatMap(_.writeToIO[Png](s"$name.png"))
+    }
+  }
+}
