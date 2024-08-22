9a4386daafcb436e691036115dece24cf711f48f
zainab-ali <zainab@kebab-ca.se>
2024-08-22 21:48:50 +0100

    Add tests for code snippet generation.

--- examples/src/test/scala/CodeSuite.scala
+++ examples/src/test/scala/CodeSuite.scala
@@ -16,12 +16,47 @@
 
 package aquascape.examples
 
-import munit.*
+import aquascape.*
 import cats.effect.IO
+import cats.syntax.all.*
 import fs2.Stream
-import snapshot4s.munit.*
+import munit.*
 import snapshot4s.generated.snapshotConfig
-class CodeSuite extends FunSuite with SnapshotAssertions {
+import snapshot4s.munit.*
+
+class CodeSuite extends CatsEffectSuite with SnapshotAssertions {
+
+  test("strips calls to stage") {
+    Scape.chunked[IO].map { scape =>
+      given Scape[IO] = scape
+      val streamCode = code {
+        Stream('a')
+          .covary[IO]
+          .stage("Stream('a')", branch = "left")
+          .fork("root", "left")
+          .merge(
+            Stream('b')
+              .evalMap(_.pure[IO].trace())
+              .evalTap(_.pure[IO].trace_())
+              .stage("Stream('b')", branch = "right")
+              .fork("root", "right")
+          )
+          .stage("merge")
+          .compile
+          .toList
+          .compileStage("compile.toList")
+      }
+      assertInlineSnapshot(
+        streamCode.code,
+        """Stream('a')
+  .covary[IO]
+  .merge(Stream('b').evalMap(_.pure[IO]).evalTap(_.pure[IO]))
+  .compile
+  .toList"""
+      )
+    }
+  }
+
   test("strips the outer braces from block elements") {
     val streamCode = code {
       val ab = Stream('a', 'b').covary[IO]
@@ -33,8 +68,7 @@ class CodeSuite extends FunSuite with SnapshotAssertions {
       """val ab = Stream('a', 'b').covary[IO]
 val xy = Stream('x', 'y').covary[IO]
 
-ab.merge(xy).compile.toList
-"""
+ab.merge(xy).compile.toList"""
     )
   }
 }
