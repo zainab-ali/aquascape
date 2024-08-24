caa5cc550eebc53a3382dfca925ba5abbd2dab49
zainab-ali <zainab@kebab-ca.se>
2024-08-24 15:50:46 +0100

    Reformat build.sbt

--- project/AquascapeDirectives.scala
+++ project/AquascapeDirectives.scala
@@ -31,11 +31,12 @@ object AquascapeDirectives extends DirectiveRegistry {
 
   // Parameter is the name of the JS instance of aquascape.example.Example
   val example: BlockDirectives.Directive =
-    // TODO: Access the cursor and 
     BlockDirectives.create("example") {
       exampleAttributes
         .mapN { case (cursor, exampleName, drawChunked, maybeSuffix) =>
-          val example = cursor.config.get[String]("pageid").fold(_ => exampleName, id => s"$id.$exampleName")
+          val example = cursor.config
+            .get[String]("pageid")
+            .fold(_ => exampleName, id => s"$id.$exampleName")
           val suffix = maybeSuffix.getOrElse("")
           val codeId = s"${example}${suffix}Code"
 
@@ -53,7 +54,9 @@ object AquascapeDirectives extends DirectiveRegistry {
     BlockDirectives.create("exampleWithInput") {
       exampleAttributes
         .mapN { case (cursor, exampleName, drawChunked, maybeSuffix) =>
-          val example = cursor.config.get[String]("pageid").fold(_ => exampleName, id => s"$id.$exampleName")
+          val example = cursor.config
+            .get[String]("pageid")
+            .fold(_ => exampleName, id => s"$id.$exampleName")
           val suffix = maybeSuffix.getOrElse("")
           val codeId = s"${example}${suffix}Code"
           val (svgEls, frameIds) = frameElsAndIds(example, drawChunked, suffix)
@@ -81,11 +84,15 @@ object AquascapeDirectives extends DirectiveRegistry {
       BlockDirectives.DirectivePart[DocumentCursor],
       BlockDirectives.DirectivePart[String],
       BlockDirectives.DirectivePart[Option[Boolean]],
-      BlockDirectives.DirectivePart[Option[String]],
-
+      BlockDirectives.DirectivePart[Option[String]]
   ) = {
     import BlockDirectives.dsl.*
-    (cursor, attribute(0).as[String], attribute("drawChunked").as[Boolean].optional, attribute("suffix").as[String].optional)
+    (
+      cursor,
+      attribute(0).as[String],
+      attribute("drawChunked").as[Boolean].optional,
+      attribute("suffix").as[String].optional
+    )
   }
   private def codeEl(codeId: String): RawContent = {
     html(
@@ -95,14 +102,18 @@ object AquascapeDirectives extends DirectiveRegistry {
 
   private def frameElsAndIds(
       example: String,
-    drawChunked: Option[Boolean],
-    suffix: String
+      drawChunked: Option[Boolean],
+      suffix: String
   ): (Seq[RawContent], String) = {
     val unchunkedFrameId = s"${example}${suffix}"
     val chunkedFrameId = s"${example}${suffix}Chunked"
-    val unchunkedSnippet = html(s"""<div id="$unchunkedFrameId" class="example-frame"></div>""")
-    val chunkedSnippet = html(s"""<div id="$chunkedFrameId" class="example-frame"></div>""")
-    val chunkedHeader =       html("<h3>chunked</h3>")
+    val unchunkedSnippet = html(
+      s"""<div id="$unchunkedFrameId" class="example-frame"></div>"""
+    )
+    val chunkedSnippet = html(
+      s"""<div id="$chunkedFrameId" class="example-frame"></div>"""
+    )
+    val chunkedHeader = html("<h3>chunked</h3>")
     drawChunked match {
       case None =>
         (
