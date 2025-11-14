a3e3f17f16e5acc9eb74e2c4e0994d9184742003
Sabrina X <sabrinakonlee@gmail.com>
2025-11-14 20:22:53 +0000

    issues-168 added more formatting

--- core/shared/src/main/scala/aquascape/ext.scala
+++ core/shared/src/main/scala/aquascape/ext.scala
@@ -26,11 +26,9 @@ import fs2.*
 
 private val defaultBranch: String = "root"
 
-
-
 extension [F[_], A: Show](s: Stream[F, A])(using t: Scape[F], ln: LineNumber) {
   def stage(label: String, branch: String = defaultBranch): Stream[F, A] = {
-    t.stage((label,ln.lineNumber), branch)(s)
+    t.stage((label, ln.lineNumber), branch)(s)
   }
 
   def fork(from: String, to: String): Stream[F, A] = t.fork(from, to)(s)
@@ -39,7 +37,9 @@ extension [F[_], A: Show](s: Stream[F, A])(using t: Scape[F], ln: LineNumber) {
 extension [F[_]: Concurrent, O: Show](fo: F[O])(using t: Scape[F]) {
   def trace(branch: String = defaultBranch): F[O] = t.trace(fo, branch)
   def trace_(branch: String = defaultBranch): F[Unit] = t.trace(fo, branch).void
-  def compileStage(label: String, branch: String = defaultBranch)(using ln: LineNumber): F[O] =
+  def compileStage(label: String, branch: String = defaultBranch)(using
+      ln: LineNumber
+  ): F[O] =
     t.compileStage(fo, (label, ln.lineNumber), branch)
 }
 
